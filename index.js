19c22c0444c6ab86d70d4b4ad6f97d106b5e0fdc
João Pedro Ougano <87842758+JPougano@users.noreply.github.com>
2022-02-15 04:40:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+function daily(){
+    var duracaoWork = document.getElementById('duracao__work');
+        duracaoWork.textContent = "5hrs";
+
+    var comparacaoWork = document.getElementById('comparacao__work');
+        comparacaoWork.textContent = "Last day - 7hrs";
+
+    var duracaoPlay = document.getElementById('duracao__play');
+        duracaoPlay.textContent = "1hr";
+
+    var comparacaoPlay = document.getElementById('comparacao__play');
+        comparacaoPlay.textContent = "Last day - 1hrs";
+
+    var duracaoStudy = document.getElementById('duracao__study');
+        duracaoStudy.textContent = "0hrs";
+
+    var comparacaoStudy = document.getElementById('comparacao__study');
+        comparacaoStudy.textContent = "Last day - 1hrs";
+
+    var duracaoExercise = document.getElementById('duracao__exercise');
+        duracaoExercise.textContent = "1hr";
+
+    var comparacaoExercise = document.getElementById('comparacao__exercise');
+        comparacaoExercise.textContent = "Last day - 1hr";
+
+    var duracaoSocial = document.getElementById('duracao__social');
+        duracaoSocial.textContent = "1hr";
+
+    var comparacaoSocial = document.getElementById('comparacao__social');
+        comparacaoSocial.textContent = "Last day - 3hrs";
+
+    var duracaoSelfCare = document.getElementById('duracao__self-care');
+        duracaoSelfCare.textContent = "0hrs";
+
+    var comparacaoSelfCare = document.getElementById('comparacao__self-care');
+        comparacaoSelfCare.textContent = "Last day - 1hrs";
+}
